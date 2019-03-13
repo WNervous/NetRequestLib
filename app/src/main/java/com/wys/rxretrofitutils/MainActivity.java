8edfdbee450c9a07536be057bc9a6c0195e79fac
WangYongSheng <m18301357350@163.com>
2019-03-13 16:54:09 +0800

    fixed

--- app/src/main/java/com/wys/rxretrofitutils/MainActivity.java
+++ app/src/main/java/com/wys/rxretrofitutils/MainActivity.java
@@ -2,6 +2,14 @@ package com.wys.rxretrofitutils;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.wys.library.bean.ComResponse;
+import com.wys.library.obsevser.CommonObserver;
+import com.wys.rxretrofitutils.net.TestApi;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TestApi.getInstance().test().getHomeBanner().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new CommonObserver<ComResponse<Object>>() {
+                    @Override
+                    public void onSuccess(ComResponse<Object> objectComResponse) {
+                        Log.e("=======>",objectComResponse.toString());
+                    }
+
+                    @Override
+                    protected void onFailed(Throwable throwable) {
+                        Log.e("=======>",throwable.toString());
+                    }
+                });
     }
 }
